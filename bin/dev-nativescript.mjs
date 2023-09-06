c848911f77409bc107bb949828830034f1a779b3
Richard van Zon <richard@relectus.com>
2023-09-06 13:50:00 +0200

    chore: removed unused package `fs`

--- bin/dev-nativescript.mjs
+++ bin/dev-nativescript.mjs
@@ -3,7 +3,6 @@ import chokidar from 'chokidar';
 import { execSync } from 'child_process';
 import prompts  from 'prompts';
 import path  from 'path';
-import fs  from 'fs';
 
 const projectDir = process.cwd();
 const buildDir = path.resolve(
